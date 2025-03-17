e255c66b51b1ce3045d58692e3fc04653a0cb7d4
Aspect <raphaelyim@gmail.com>
2025-03-16 22:44:05 -0600

    Actual Implementations

--- src/main/java/cards/apparatus/TorsionBalance.java
+++ src/main/java/cards/apparatus/TorsionBalance.java
@@ -1,5 +1,6 @@
 package cards.apparatus;
 
+import cards.scientists.forces_and_fields.Coulomb;
 import components.AbstractCard;
 import components.Player;
 import enums.CardTypes;
@@ -10,11 +11,13 @@ import java.util.HashMap;
 // Torsion Balance - F&F Apparatus | 1 Electron
 // Timeframe {1785,1785}
 // On upkeep, if you do not have a scientist named [Coulomb] on your field, destroy this card.
-// Whenever 1 or more [Coulomb] attacks, gain 1 Charge.
-// 7 Charge: Multiply all scientists named [Coulomb] on your field's attack by 8.99E9 for 1 turn.
-//           For the rest of the turn, [Coulomb] cannot inflict battle damage on a player.
+// Whenever [Coulomb] attacks, gain 1 Charge.
+// Charge >= 8.99: Increase the attack of all Coulomb cards by 9.
+// 8.99E9
 
 public class TorsionBalance extends AbstractCard {
+
+    private int charge;
 
     public TorsionBalance(HashMap<ParticleTypes, Integer> castingCost, String name, int[] timeframe, CardTypes cardType) {
         super(
@@ -31,5 +34,16 @@ public class TorsionBalance extends AbstractCard {
     public void destroyEffect() {}
 
     @Override
-    public void triggerEffect() {}
+    public void triggerEffect() {
+        charge += 1;
+        if (charge == 9) {
+            owner.getField().forEach(
+                    (card -> {
+                        if (card instanceof Coulomb coulomb) {
+                            coulomb.setAttack(coulomb.getAttack()+9);
+                        }
+                    })
+            );
+        }
+    }
 }
